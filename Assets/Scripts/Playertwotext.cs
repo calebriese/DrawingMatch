474845918226a3147d4daf564229eb2b56ab3127
Caleb Riese <calebriese@outlook.com>
2020-12-16 11:22:40 -0600

    Added shapes as assets

--- Assets/Scripts/Playertwotext.cs
+++ Assets/Scripts/Playertwotext.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class Playertwotext : MonoBehaviour
 {
-public string theText;
-public GameObject inputfield;
-public GameObject textDisplay;
+    public string theText;
+    public GameObject inputfield;
+    public GameObject textDisplay;
 
-public void TextEnter()
-{
-const int MAX_Len=21;
-string theText = string.Empty;
-theText=inputfield.GetComponent<Text>().text;
-if(theText.Length > MAX_Len)
-    theText = theText.Substring(0,MAX_Len -1);
-textDisplay.GetComponent<Text>().text = "Player 2 said: " + theText;
-}
+    public class Message : MessageBase
+    {
+        public string theMessage;
+    }
+
+    public void TextEnter()
+    {
+        const int MAX_Len=21;
+        string theText = string.Empty;
+        theText=inputfield.GetComponent<Text>().text;
+        if(theText.Length > MAX_Len)
+            theText = theText.Substring(0,MAX_Len -1);
+        textDisplay.GetComponent<Text>().text = "Player 2 said: " + theText;
+
+        Message myMessage = new Message();
+        myMessage.theMessage = "Player 2 said: " + theText;
+        NetworkServer.SendToAll( 6969 , myMessage);
+    }
 }
